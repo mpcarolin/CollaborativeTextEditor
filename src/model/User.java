3451ce31fe3bb44005ce61bc5f1f47e8e90d3a59
davetian25 <davetian@email.arizona.edu>
2015-11-06 16:52:59 -0700

    Added user

--- src/model/User.java
+++ src/model/User.java
@@ -1,6 +1,48 @@
 package model;
+import java.io.Serializable;
+import java.util.List;
 
-public class User {
+public class User implements Serializable {
 
-	//TODO user class
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4807570774011333657L;
+	private String username;
+	private String password;
+	private String id;
+	private List<Object> permissions;
+	
+	public User (String newUser, String pass, String newID, Object docPerm) {
+		this.username = newUser;
+		this.password = pass;
+		this.id = newID;
+		permissions.add(docPerm);
+	}
+	
+	public String getName() {
+		return username;
+	}
+	
+	public String getIDNum() {
+		return id;
+	}
+	
+	public boolean hasPermission(Object document) {
+		for(Object o: permissions) {
+			if(o.equals(document))
+				return true;
+		}
+		return false;
+	}
+	
+	public void givePermission(Object document) {
+		permissions.add(document);
+	}
+	
+	public void changePassword(String newPass) {
+		password = newPass;
+	}
+	
 }
+
