198ea7b9918a63f3969c42d41833a05dcf8d7c3c
Michael <mpcarolin@gmail.com>
2015-11-10 18:31:07 -0700

    Changed document object to store a stack of strings instead of a stack of docs

--- src/model/Document.java
+++ src/model/Document.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class Document {
 	
 	private static final int NUM_REVISIONS_STORED = 20;
-	private Stack<Document> history = new Stack<Document>();
+	private Stack<String> history = new Stack<String>();
 	
 	private String currentText;
 	
@@ -14,28 +14,30 @@ public class Document {
 	}
 	
 	public void replaceText(String newText) {
-		addToHistory(this);
 		currentText = newText;
+		addToHistory(currentText);
 	}
 	
 	public void append(String textToAppend) {
-		addToHistory(this);
 		currentText = currentText + textToAppend; 
+		addToHistory(currentText);
 	}
 	
 	public String getText() {
 		return currentText;
 	}
 	
-	public void revertToLastRevision() {
+	public void getLastRevision() {
+		currentText = history.pop();
 	}
 	
 	// pushes to history, but manages the stack if length is beyond the 
 	// num_revisions_stored limit
-	private void addToHistory(Document lastRevision) {
+	private void addToHistory(String lastRevision) {
 		if (history.size() >= NUM_REVISIONS_STORED) {
 			history.remove(0);
 		}
+		
 		history.push(lastRevision);
 	}
 	
