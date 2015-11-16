2d5a48a63fe1966c679c37e6921071908759ef0a
Daniel Decina <decina@email.arizona.edu>
2015-11-15 23:34:25 -0700

    Made the following changes to server: Changed all synchonized lists/maps shared by all handlers to static. Updated the login and createAccount methods to use ServerResponse enums, and perform addtional error checking. Added functionality to keep track of the current user (also added a couple methods to User). Started functionality to create new documents. Did general refactoring, changed several varible names for improved self documentation and readability. Attempted to update all comments.

--- src/model/User.java
+++ src/model/User.java
@@ -4,50 +4,60 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Random;
 
+@SuppressWarnings("serial")
 public class User implements Serializable {
 
-	/**
-	 * store passwords as a hash create user, generate unique/random id cast
-	 * integer to string, concat to password at beginning hash it, store it, log
-	 * into server to server -> get id -> get account -> get password check to
-	 * make sure these are the same id+password hash stored 8 digits
-	 */
-	private static final long serialVersionUID = 4807570774011333657L;
-	private String username;
-	private int id;
-	private int hashPass;
-	private List<Object> documentsOwned;
-	private List<Object> editableDocuments;
+   /**
+    * store passwords as a hash create user, generate unique/random id cast
+    * integer to string, concat to password at beginning hash it, store it, log
+    * into server to server -> get id -> get account -> get password check to
+    * make sure these are the same id+password hash stored 8 digits
+    */
+   private String username;
+   private int id;
+   private int hashPass;
+   private boolean loggedIn;
+   private List<Object> documentsOwned;
+   private List<Object> editableDocuments;
 
-	public User(String newUser, String pass) {
-		this.username = newUser;
-		setPassword(pass);
-	}
+   public User(String newUser, String pass) {
+      this.username = newUser;
+      setPassword(pass);
+   }
 
-	public void setPassword(String pass) {
-		Random rng = new Random();
-		this.id = rng.nextInt(90000000);
-		this.hashPass = (id + pass).hashCode();
-	}
+   public void setPassword(String pass) {
+      Random rng = new Random();
+      this.id = rng.nextInt(90000000);
+      this.hashPass = (id + pass).hashCode();
+      this.loggedIn = false;
+   }
 
-	public String getName() {
-		return username;
-	}
+   public String getName() {
+      return username;
+   }
 
-	public int getID() {
-		return id;
-	}
+   public int getID() {
+      return id;
+   }
 
-	public int getHashPass() {
-		return hashPass;
-	}
+   public int getHashPass() {
+      return hashPass;
+   }
 
-	public boolean hasPermission(Object document) {
-		return documentsOwned.contains(document) || editableDocuments.contains(document);
-	}
+   public void setLogin(boolean status) {
+      loggedIn = status;
+   }
 
-	public void givePermission(Object document) {
-		editableDocuments.add(document);
-	}
+   public boolean isLoggedIn() {
+      return loggedIn;
+   }
+
+   public boolean hasPermission(Object document) {
+      return documentsOwned.contains(document) || editableDocuments.contains(document);
+   }
+
+   public void givePermission(Object document) {
+      editableDocuments.add(document);
+   }
 
 }
