84ebdef24d086feed21917014802c4d9bcefa16a
Michael <mpcarolin@gmail.com>
2015-11-16 15:26:59 -0700

    Added some more user-management functionality to document class

--- src/model/Document.java
+++ src/model/Document.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Document {
@@ -7,8 +8,14 @@ public class Document {
 	private static final int NUM_REVISIONS_STORED = 20;
 	private Stack<Revision> history; 
 	private String currentText;
+	private String documentName;
+	private String ownerName;
+	private LinkedList<String> editorNames;
 	
-	public Document() {
+	public Document(String documentName, String ownerName) {
+		this.documentName = documentName;
+		this.ownerName = ownerName;
+		editorNames.add(ownerName);
 		history = new Stack<Revision>();
 		currentText = "";
 	}
@@ -29,6 +36,10 @@ public class Document {
 		currentText = currentText + textToAppend; 
 	}
 	
+	public void addEditor(String editorUsername) {
+		editorNames.add(editorUsername);
+	}
+	
 	public String getText() {
 		return currentText;
 	}
@@ -46,5 +57,15 @@ public class Document {
 
 		Revision revision = new Revision(newText, currentText, revisingUser); 
 		history.push(revision);
+	}
+	
+	
+	
+	public String getOwner() {
+		return ownerName;
+	}
+	
+	public LinkedList<String> getEditors() {
+		return editorNames;
 	}
 }
---
+++ src/model/ServerCommand.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum ServerCommand {
+	CHAT_MSG,  DOC_TEXT, 
+	OPEN_DOC, CLOSE_DOC, CREATE_DOC, 
+	REVERT, LOGOUT, LOGIN, CREATE_ACCOUNT;  
+}
