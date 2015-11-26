8fcc902c56744dd3f1a0ccbac4bfe30cd8117e47
Daniel Decina <decina@email.arizona.edu>
2015-11-25 19:44:50 -0700

    team recovery... maybe

--- src/model/Server.java
+++ src/model/Server.java
@@ -413,13 +413,6 @@ class ClientHandler extends Thread {
    }
 
    /*
-    * Saves a revision.
-    */
-   private void saveRevision() {
-      currentOpenDoc.saveRevision(currentUser.getName());
-   }
-
-   /*
     * Closes the current OpenDocument.
     */
    private void closeDocument() {
