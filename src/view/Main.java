5934da157c9b0738bdb2d65665bbab3b0eab8597
jacobgroh <jacobgroh@email.arizona.edu>
2015-11-29 22:36:05 -0700

    Doc listener implemented

--- src/view/Main.java
+++ src/view/Main.java
@@ -10,8 +10,8 @@ import model.Server;
 public class Main {
 
 
-   private static final String ADDRESS = "helen.twilightparadox.com";
-   //private static final String ADDRESS = "localhost";
+  // private static final String ADDRESS = "helen.twilightparadox.com";
+   private static final String ADDRESS = "localhost";
 
    public static void main(String[] args) {
 
