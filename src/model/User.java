2dfa8c0a11001c06db872651643b81edbfecfe9f
Daniel Decina <decina@email.arizona.edu>
2015-12-07 15:46:54 -0700

    removed public access modifiers and did general refactoring

--- src/model/User.java
+++ src/model/User.java
@@ -2,7 +2,6 @@ package model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -16,12 +15,12 @@ public class User implements Serializable {
     private List<String> ownedDocuments;
     private List<String> editableDocuments;
 
-    public User(String newUser, String pass) {
+    User(String newUser, String pass) {
 	this.username = newUser;
 	setPassword(pass);
     }
 
-    public void setPassword(String pass) {
+    void setPassword(String pass) {
 	Random rng = new Random();
 	salt = rng.nextInt(90000000);
 	hashPass = (salt + pass).hashCode();
@@ -29,53 +28,53 @@ public class User implements Serializable {
 	editableDocuments = new ArrayList<String>();
     }
 
-    public String getName() {
+    String getName() {
 	return username;
     }
 
-    public int getSalt() {
+    int getSalt() {
 	return salt;
     }
 
-    public int getHashPass() {
+    int getHashPass() {
 	return hashPass;
     }
 
-    public void setLogin(boolean status) {
+    void setLogin(boolean status) {
 	loggedIn = status;
     }
 
-    public boolean isLoggedIn() {
+    boolean isLoggedIn() {
 	return loggedIn;
     }
 
-    public boolean owns(String documentName) {
+    boolean owns(String documentName) {
 	return ownedDocuments.contains(documentName);
     }
 
-    public boolean hasPermission(String documentName) {
+    boolean hasPermission(String documentName) {
 	return ownedDocuments.contains(documentName) || editableDocuments.contains(documentName);
     }
 
-    public void addOwnedDocument(String documentName) {
+    void addOwnedDocument(String documentName) {
 	ownedDocuments.add(documentName);
 	editableDocuments.add(documentName);
     }
 
-    public void addEditableDocument(String documentName) {
+    void addEditableDocument(String documentName) {
 	editableDocuments.add(documentName);
     }
 
-    public void removeDocument(String documentName) {
+    void removeDocument(String documentName) {
 	ownedDocuments.remove(documentName);
 	editableDocuments.remove(documentName);
     }
 
-    public List<String> getOwnedDocuments() {
+    List<String> getOwnedDocuments() {
 	return ownedDocuments;
     }
 
-    public List<String> getEditableDocuments() {
+    List<String> getEditableDocuments() {
 	return editableDocuments;
     }
 }
